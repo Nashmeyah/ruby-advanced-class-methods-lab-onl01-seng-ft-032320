c55dd8900d25ea888a64c8c157000586136a5d56
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-03 15:44:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,12 +47,18 @@ class Song
 
   def self.new_from_filename(data)
     data.delete_suffix!(".mp3")
-    new_array = data.split(" - ")
-
-    song = self.new
-    song.name = new_array[1]
-    song.artist_name = new_array[0]
-    song
+       new_array = data.split(" - ")
+       @name.name = new_array[1]
+       @name = self.create
+       @name.artist_name = new_array[0]
+    
+    # data.delete_suffix!(".mp3")
+    # new_array = data.split(" - ")
+    # 
+    # song = self.new
+    # song.name = new_array[1]
+    # song.artist_name = new_array[0]
+    # song
   # rows = data.split(/ - |.mp3/)
   # name = rows[1]
   # artist_n = rows[0]
